32821e71216548355c11a836972864858a5807e8
Benjamin <benjamin.martel.98@gmail.com>
2023-09-23 00:31:28 +0200

    bugfix ui +sorting moves on pokemon detail

--- pokedex/src/app/pokedex/pokemon-detail/pokemon-move-tab/pokemon-move-tab.component.ts
+++ pokedex/src/app/pokedex/pokemon-detail/pokemon-move-tab/pokemon-move-tab.component.ts
@@ -12,4 +12,20 @@ export class PokemonMoveTabComponent {
   constructor() {
     this.pokemon = {} as PokemonDetail;
   }
+
+  getSortedPokemonMove(){
+    return this.pokemon.moves.sort(function IHaveAName(a, b) { // non-anonymous as you ordered...
+                                    var lvlA = a.version_group_details[a.version_group_details.length-1].level_learned_at
+                                    var lvlB = b.version_group_details[b.version_group_details.length-1].level_learned_at
+                                    var methodA = a.version_group_details[a.version_group_details.length-1].move_learn_method.name
+                                    var methodB = b.version_group_details[b.version_group_details.length-1].move_learn_method.name
+                                    return methodB == "level-up" && methodB != methodA ?  1 // if b should come earlier, push a to end
+                                         : methodA == "level-up" && methodB != methodA ? -1 // if b should come later, push a to begin
+                                         : lvlB < lvlA ?  1 // if b should come earlier, push a to end
+                                         : lvlB > lvlA ? -1 // if b should come later, push a to begin
+                                         : methodB < methodA ?  1 // if b should come earlier, push a to end
+                                         : methodB > methodA ? -1 // if b should come later, push a to begin
+                                         : 0;                   // a and b are equal
+                                });;
+  }
 }
